9834f43d21cbd637be0b7a49bdd6910e9d21d8d5
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-03-03 16:34:27 +0100

    semi end js

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,26 @@
 fetch("https://lanciweb.github.io/demo/api/pictures/")
     .then(response => response.json())
+
     .then(data => {
-        console.log(data.title);
+        for (let i = 0; i < data.length; i++) {
+            const dataEl = data[i];
+            console.log(dataEl);
+
+            const changeImg = document.querySelectorAll("img")
+            changeImg[i].src = dataEl.url
+
+            const changeDate = document.querySelectorAll(".date")
+            changeDate[i].innerHTML = dataEl.date
+
+            const changeTitle = document.querySelectorAll(".title_card")
+            changeTitle[i].innerHTML = toUpperChase(dataEl.title)
+
+
+
+
+
+
+        }
     })
     .catch(error => {
         console.error(error);

f355425f6840e2c34c13d01f468394fd768139ab
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-03-04 08:29:26 +0100

    end ex pt 2

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,36 @@
 fetch("https://lanciweb.github.io/demo/api/pictures/")
     .then(response => response.json())
-
     .then(data => {
-        for (let i = 0; i < data.length; i++) {
-            const dataEl = data[i];
-            console.log(dataEl);
+        const cards = document.querySelectorAll(".cards");
+        const changeImg = document.querySelectorAll(".img_cards");
+        const changeDate = document.querySelectorAll(".date");
+        const changeTitle = document.querySelectorAll(".title_card");
 
-            const changeImg = document.querySelectorAll("img");
-            changeImg[i].src = dataEl.url;
 
-            const changeDate = document.querySelectorAll(".date");
-            changeDate[i].innerHTML = dataEl.date;
+        const overlay = document.getElementById("overlay");
+        const overlayImg = document.getElementById("over_img");
+        const closeButton = document.getElementById("close");
 
-            const changeTitle = document.querySelectorAll(".title_card")
-            changeTitle[i].innerHTML = dataEl.title.toUpperCase()
-        }
+        data.forEach((dataEl, i) => {
+            if (i < cards.length) {
+                changeImg[i].src = dataEl.url;
+                changeDate[i].innerHTML = dataEl.date;
+                changeTitle[i].innerHTML = dataEl.title.toUpperCase();
+
+
+                cards[i].addEventListener('click', function () {
+                    console.log("Cliccata card:", dataEl.title);
+                    overlay.style.display = "flex";
+                    overlayImg.src = dataEl.url;
+                });
+            }
+        });
+
+
+        closeButton.addEventListener('click', function () {
+            overlay.style.display = "none";
+        });
     })
     .catch(error => {
-        console.error(error);
+        console.error("Errore nel caricamento dei dati:", error);
     });
